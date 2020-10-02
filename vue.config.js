63c29d2a478202c23c5cdf6222270cec76f454ed
Paul <5748299+biggestcookie@users.noreply.github.com>
2020-10-02 10:11:48 -0400

    Project structure, bulma install

--- vue.config.js
+++ vue.config.js
@@ -20,5 +20,13 @@ module.exports = {
         }
       }
     }
+  },
+  css: {
+    sourceMap: process.env.NODE_ENV !== "production",
+    loaderOptions: {
+      sass: {
+        prependData: '@import "@/assets/styles/_variables.scss";'
+      }
+    }
   }
 };
